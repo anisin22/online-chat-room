c22dfb343b4a6140ab83495a20c0a1636172274a
anisin22 <anish.singla@mail.utoronto.ca>
2020-08-31 19:14:15 -0600

    v1

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+const { Socket } = require('dgram');
+const PORT = 3000 || process.env.PORT;
+const server = http.createServer(app);
+const io = socketio(server);
+const formatMessage = require('./utils/messages');
+const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require('./utils/users');
+const botName = 'Chat Room Bot';
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', socket => {
+
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        socket.join(user.room);
+        
+        socket.emit('message', formatMessage(botName,'Welcome to the Chat Room!'));
+
+        socket.broadcast.to(user.room).emit('message', formatMessage(botName,`${user.username} has joined the Room`));
+
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+    });
+
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit('message', formatMessage(user.username,msg));
+    });
+
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', formatMessage(botName,`${user.username} has left the room`));
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+        }
+        
+    });
+});
+
+server.listen(PORT, () => console.log(`Server Live on port ${PORT}`));
